de42d50e0a04cf34dd466c1976aa00c4ac9a152e
Gustavo Vergara <gustavo@fulllab.com.br>
2018-07-12 19:30:03 -0300

    Initial Commit

---
+++ Task/Task.h
@@ -0,0 +1,19 @@
+//
+//  Task.h
+//  Task
+//
+//  Created by Gustavo Vergara Garcia on 12/07/18.
+//  Copyright © 2018 Gustavo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Task.
+FOUNDATION_EXPORT double TaskVersionNumber;
+
+//! Project version string for Task.
+FOUNDATION_EXPORT const unsigned char TaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Task/PublicHeader.h>
+
+
